fbf2fe9b5b1c2c98aa31cc8603cf2e019764c9d7
boscotsang <boscotsang3@gmail.com>
2015-11-20 21:08:49 +0800

    add gevent cocurrency

--- script/best_every_day.py
+++ script/best_every_day.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python                                                               
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from gevent import monkey
+monkey.patch_socket()
+import gevent
 import requests
 import json
 import logging
@@ -11,6 +14,7 @@ class BestEveryDay(object):
 
     STOCK_PRICE_URL = "http://ifzq.gtimg.cn/appstock/app/fqkline/get?p=1&param=%s,day,,,320,qfq"
     BOLL_TECH_ANAL = "http://ifzq.gtimg.cn/appstock/indicators/BOLL/D1?market=%s&code=%s&args=20&start=&end=&limit=320&fq=qfq"
+    MACD_TECH_ANAL = "http://ifzq.gtimg.cn/appstock/indicators/MACD/D1?market=%s&code=%s&args=20&start=&end=&limit=320&fq=qfq"
     #http://baike.baidu.com/view/965844.htm
     SZ_A =  {
         "code": "000%03d",
@@ -38,20 +42,15 @@ class BestEveryDay(object):
     def __init__(self):
         logging.basicConfig(filename='best.log', format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
         logging.getLogger('requests').setLevel(logging.ERROR)
-        date = "%s" % datetime.datetime.today().date() 
+        date = "%s" % datetime.datetime.today().date()
         self.json_file = file("../data/stock_every_day/%s.json" % date, "w")
         self.html_file = file("../data/stock_every_day/%s.html" % date, "w")
         self.best = []
 
-    def find_best(self, market):
-        for i in xrange(1, 1000):
-            stock_num = market['code'] % i
-            full_code = market['full_code'] % i
-            stock_price_url = self.STOCK_PRICE_URL % full_code
-            boll_url = self.BOLL_TECH_ANAL % (market['market'], stock_num)
+    def fetch(self, stockurl, bollurl, stock_num, full_code):
             try:
-                stock_info = requests.get(stock_price_url, timeout=self.TIMEOUT).json()
-                boll_info = requests.get(boll_url, timeout=self.TIMEOUT).json()
+                stock_info = requests.get(stockurl, timeout=self.TIMEOUT).json()
+                boll_info = requests.get(bollurl, timeout=self.TIMEOUT).json()
                 last_day_boll = boll_info['data'][-2]
                 last_day_price = stock_info['data'][full_code]['qt'][full_code][3]
                 name = stock_info['data'][full_code]['qt'][full_code][1]
@@ -61,7 +60,7 @@ class BestEveryDay(object):
                 if rate <= self.LOW_RATE and name.find("ST") == -1 and name.find(u"退市") == -1:
                     logging.info('get -- (%s, %s, price: %s, rate: %s )' % (name, stock_num, last_day_price, rate))
                     self.best.append({
-                        "name" : name, 
+                        "name" : name,
                         "stock_code" : stock_num,
                         "current_price" : last_day_price,
                         "price_to_low_boll_rate": rate
@@ -69,6 +68,11 @@ class BestEveryDay(object):
             except Exception as e:
                 logging.info("error %s" % e)
                 pass
+
+    def find_best(self, market):
+        stocks = [(market['code'] % i, market['full_code'] % i) for i in xrange(1000)]
+        urls = [(self.STOCK_PRICE_URL % full_code, self.BOLL_TECH_ANAL % (market['market'], stock_num), stock_num, full_code) for stock_num, full_code in stocks]
+        gevent.joinall([gevent.spawn(self.fetch, stock_price_url, boll_url, stock_num, full_code) for (stock_price_url, boll_url, stock_num, full_code) in urls])
 
     def start(self):
         self.find_best(self.SZ_A)
